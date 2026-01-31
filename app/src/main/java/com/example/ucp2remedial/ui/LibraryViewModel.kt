4ec03f6c327a3a9138e183add1b55579701fa940
hendr1an <hendriansyahgyan@gmail.com>
2026-01-31 12:40:42 +0700

    Finishing

--- app/src/main/java/com/example/ucp2remedial/ui/LibraryViewModel.kt
+++ app/src/main/java/com/example/ucp2remedial/ui/LibraryViewModel.kt
@@ -22,14 +22,14 @@ class LibraryViewModel(private val repository: LibraryRepository) : ViewModel() 
     val authors: StateFlow<List<Author>> = repository.getAllAuthors()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 
-    // Tambahkan validasi cyclic sederhana
+
     fun addCategory(name: String, parentId: Long?) {
         viewModelScope.launch {
-            repository.addCategory(name, parentId) // Pastikan repo juga terima parentId
+            repository.addCategory(name, parentId)
         }
     }
 
-    // Update fungsi ini menerima List Author
+
     fun addBook(title: String, status: String, categoryId: Long?, authorIds: List<Long>) {
         viewModelScope.launch {
             repository.addBook(title, status, categoryId, authorIds)
@@ -44,7 +44,7 @@ class LibraryViewModel(private val repository: LibraryRepository) : ViewModel() 
             try {
                 repository.deleteCategory(categoryId, deleteBooks)
             } catch (e: Exception) {
-                // Handle error
+
             }
         }
     }
